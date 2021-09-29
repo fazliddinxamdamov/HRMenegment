569ce28e269c4085f47dbffb2156fa24c56c9b65
Fazliddin Xamdamov <fazliddinxamdamov102@gmail.com>
2021-09-29 12:09:23 +0500

    first commit

---
+++ src/main/java/pdp/uz/hrmenegment/HrMenegmentApplication.java
@@ -0,0 +1,13 @@
+package pdp.uz.hrmenegment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrMenegmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrMenegmentApplication.class, args);
+    }
+
+}
